dcd8bfeff92e2cb9b9ae68ada41b2f57ffbc7449
chosey85 <Sxsbsy123!>
2024-03-28 19:26:38 +0200

    created banner func

--- morpheus_main.py
+++ morpheus_main.py
@@ -1,10 +1,15 @@
-import pyfiglet
 from colorama import Fore
+import pyfiglet
 
-ascii_art = pyfiglet.figlet_format("Morpheus")
-colored_ascii_art = Fore.YELLOW + ascii_art + Fore.RESET
-colored_line = Fore.YELLOW + '- - - - ------------------------------ - - - -' + Fore.RESET
-installer_text = Fore.RED + '               I N S T A L L E R' + Fore.RESET
-print(colored_ascii_art)
-print(installer_text)
-print(colored_line)
+def morpheus_banner():
+    ascii_art = pyfiglet.figlet_format("Morpheus")
+    colored_ascii_art = Fore.YELLOW + ascii_art + Fore.RESET
+    colored_line = Fore.YELLOW + '- - - - ------------------------------ - - - -' + Fore.RESET
+    installer_text = Fore.RED + '               I N S T A L L E R' + Fore.RESET
+    print(colored_ascii_art)
+    print(installer_text)
+    print(colored_line)
+
+# Call the function to display the Morpheus banner
+morpheus_banner()
+
